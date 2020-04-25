5eb8e3f6740b042ab98926bc951d3f9907f6d5fb
tyler4400 <tyler4400@foxmail.com>
2020-04-25 21:09:38 +0800

    4-10button组件样式

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-	      <h1>Hello world</h1>
-	      <h2>Hello world</h2>
-	      <h3>Hello world</h3>
-	      <hr/>
-	      <code>
-		      const a = 'b'
-	      </code>
-	      <Button disabled> Hello </Button>
-	      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Hello </Button>
-	      <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled> Baidu Link </Button>
-
-	      <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+	      <Button> Hello </Button>
+	      <Button disabled> Disabled Button </Button>
+	      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
+	      <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
+	      <Button btnType={ButtonType.Link} href="http://www.baidu.com"> Baidu Link </Button>
+	      <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled> Disabled Link </Button>
       </header>
     </div>
   );
