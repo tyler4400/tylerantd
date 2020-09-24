58455d31cfa4148f51ca86d6b4f64762e5e46ae1
tylerzzheng <tylerzzheng@tencent.com>
2020-09-24 10:04:53 +0800

    6-3 需求升级 - Menu 组件编码第二部分

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import Button, {ButtonSize, ButtonType} from "./components/Button/button";
+import Menu from "./components/Menu/menu";
+import MenuItem from "./components/Menu/menuItem";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+	      <Menu defaultIndex={0} onSelect={(index) => {alert(index)}} mode="vertical">
+		      <MenuItem index={0}>
+			      cool link
+		      </MenuItem>
+		      <MenuItem index={1} disabled>
+			      disabled llink
+		      </MenuItem>
+		      <MenuItem index={2}>
+			      cool link 3
+		      </MenuItem>
+	      </Menu>
+
 	      <Button className="custom"> Hello </Button>
 	      <Button disabled> Disabled Button </Button>
 	      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
--- src/components/Menu/menuItem.tsx
+++ src/components/Menu/menuItem.tsx
@@ -1,23 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import classNames from 'classnames'
+import { MenuContext } from "./menu";
 
 export interface MenuItemProps {
-  index?: number;
-  disabled?: boolean;
-  className?: string;
-  style?: React.CSSProperties;
+	index: number;
+	disabled?: boolean;
+	className?: string;
+	style?: React.CSSProperties;
 }
 
 const MenuItem: React.FC<MenuItemProps> = (props) => {
-  const { index, disabled, className, style, children } = props
-  const classes = classNames('menu-item', className, {
-    'is-disabled': disabled,
-  })
-  return (
-    <li className={classes} style={style}>
-      {children}
-    </li>
-  )
+	const { index, disabled, className, style, children } = props
+	const context = useContext(MenuContext)
+	const classes = classNames('menu-item', className, {
+		'is-disabled': disabled,
+		'is-active': context.index === index
+	})
+	const handleClick = () => {
+		if(context.onSelect && !disabled) {
+			context.onSelect(index)
+		}
+	}
+
+	return (
+		<li className={classes} style={style} onClick={handleClick}>
+			{children}
+		</li>
+	)
 }
 
 export default MenuItem
